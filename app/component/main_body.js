06ddc00343db754214bcfdcc19edce461cddcf6f
Abdullah Rahman <abdullah2891@gmail.com>
2017-12-17 17:27:25 +0000

    lists of posts

--- app/component/main_body.js
+++ app/component/main_body.js
@@ -1,16 +1,53 @@
 import React , {Component} from 'react'; 
 
-import { Text, View } from 'react-native';
+import {Alert,Text, View, ScrollView} from 'react-native';
 
+import {List, ListItem} from 'react-native-elements';
 
 export default class MainBody extends Component{
     
+    constructor(props){
+        super(...arguments); 
+        
+        this.state = {
+            posts  : []
+        }
+    }
+    
+    componentDidMount(){
+        fetch('https://www.reddit.com/r/changemyview/.json')
+            .then(response => response.json() )
+            .then(response=>{
+                let posts = response.data.children; 
+                
+                this.setState({posts : posts});
+            })
+    }
+    
+    _on_click(item){
+        Alert.alert('You tapped the button!');
+    }
+        
     render(){
+
         
         return(
-                <View>
-                    <Text> test </Text>
-                </View>
+            
+                <ScrollView>
+                    <List>
+                      {
+                        this.state.posts.map((item, i) => (
+                          <ListItem
+                            key={i}
+                            title={item.data.title}
+                            titleNumberOfLines = {3}
+                            onPress = {()=>this._on_click(item)}
+                          />
+                        ))
+                      }
+                    </List>
+                </ScrollView>
+                
             
             );
     }
